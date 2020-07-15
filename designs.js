0093c4a70729ae3e23e6b364d994e0e8acef404b
Tiina Aardemae <tiina.aardemae@gmail.com>
2020-07-15 11:13:25 -0700

    Addressed reviewer feedback, updated ColorPicker code

--- designs.js
+++ designs.js
@@ -1,24 +1,28 @@
-// Select color input
+// Commenting out the following color input code, which I had in originally.
+// For some reason, this method was working in my browser, but not working in
+// the reviewer's browser. Updated color input code added to Row 45 onward.
 
-var color = "";
-document.getElementById("pixelCanvas").addEventListener("click", function (event) {
-    var colorElement = document.getElementById("colorPicker");
-    var color = colorElement.value;
-    var click = event.target.id;
-    document.getElementById(click).style.backgroundColor = color;
-})
+// var color = "";
+// document.getElementById("pixelCanvas").addEventListener("click", function (event) {
+//     var colorElement = document.getElementById("colorPicker");
+//     var color = colorElement.value;
+//     var click = event.target.id;
+//     document.getElementById(click).style.backgroundColor = color;
+// })
 
 // Select size input
 
+const row_input = document.getElementById('inputHeight');
+const column_input = document.getElementById('inputWidth');
 var size = document.querySelector("#sizePicker"); 
 size.addEventListener("submit", function(event) {
     event.preventDefault();
-    var height = document.getElementById("inputHeight").value;
-    var width = document.getElementById("inputWidth").value;
+    let row_count = row_input.value;
+    let column_count = column_input.value;
 
 // When size is submitted by the user, call makeGrid()
 
-    makeGrid(height, width);
+    makeGrid(row_count, column_count);
 })
 
 function makeGrid(height, width) {
@@ -37,4 +41,15 @@ function makeGrid(height, width) {
             line.insertAdjacentHTML("afterbegin",`<td id = cell${i}${j}></td>`);
         }
     }
+
+// Select color; clickListener moved from table to td element, 
+// per reviewer comment.
+
+    let tableCells = Array.from(document.querySelectorAll("td"));
+
+    tableCells.forEach(function(cell) {
+        cell.addEventListener("click", function() {
+            this.style.backgroundColor = document.querySelector("#colorPicker").value;
+        }, false);
+    })
 }
